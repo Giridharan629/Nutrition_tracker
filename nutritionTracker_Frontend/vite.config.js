ab4d2a59f212903dc99dc580c3c747656679b294
Giridharan C <118421351+Giridharan629@users.noreply.github.com>
2024-12-15 22:41:14 +0530

    vite config

--- nutritionTracker_Frontend/vite.config.js
+++ nutritionTracker_Frontend/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Read the VITE_API_URL from environment variables
+const apiUrl = process.env.VITE_API_URL || 'http://localhost:8000'; // Default to localhost if not defined
+
 export default defineConfig({
   plugins: [react()],
   server: {
-      proxy: {
-          '/track': {
-              target: 'http://localhost:8000',
-              changeOrigin: true,
-              secure: false
-          }
-      }
-  }
+    proxy: {
+      '/track': {
+        target: apiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
